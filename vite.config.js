02d5bd4ac6ee79d1f87bea00daf7104d3e5d56b2
Uzair Danayal <muzaird@gmail.com>
2025-07-06 11:58:42 +0500

    revert htaccess

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Or '/your-sub-directory/' if hosted under a subfolder
   plugins: [react()],
-});
+})
